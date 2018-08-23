d92cae6ee49318edfabcef9a0668baf180f7f765
Carlos Llanos <carlos.llanos@qrvey.com>
2018-08-23 17:26:54 -0500

    Updated multiple answers services to match new cycle method

--- .eslintrc.js
+++ .eslintrc.js
@@ -102,7 +102,7 @@ module.exports = {
         "brw": true,
         "env": true,
         "user": true,
-        "do": true,
+        "dos": true,
         "chai": true,
         "server": true,
         "protractor": true,
